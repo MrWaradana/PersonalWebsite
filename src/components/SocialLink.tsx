da570372844b2f3a95169305e9bceaf84c1c18eb
MrWaradana <muhammadridhowaradana@gmail.com>
2023-07-20 19:45:27 +0700

    feat: responsive social links

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -2,7 +2,7 @@ import { FaLinkedin, FaGithub } from 'react-icons/fa'
 
 export default function SocialLink() {
     return (
-        <div className='flex gap-12'>
+        <div className='flex flex-col gap-6 lg:gap-12 lg:flex-row'>
             {/* github */}
             <a href="https://github.com/MrWaradana" className='flex flex-row gap-2 text-sm font-semibold lg:text-lg group min-w-fit'><FaGithub className='translate-y-1 ease group-hover:-translate-y-[.5px]' />MrWaradana</a>
             {/* twitter */}
