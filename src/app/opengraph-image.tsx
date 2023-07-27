d28069605c8e9acaea5c3b653be3e017e51c8c91
MrWaradana <muhammadridhowaradana@gmail.com>
2023-07-27 18:23:31 +0700

    fix: og image font

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -13,9 +13,9 @@ export const size = {
 export const contentType = 'image/png'
 
 // Font
-const interSemiBold = fetch(
-    new URL('../../public/Inter-SemiBold.ttf', import.meta.url)
-).then((res) => res.arrayBuffer())
+// const interSemiBold = fetch(
+//     new URL("../../public/Inter-SemiBold.ttf", import.meta.url)
+// ).then((res) => res.arrayBuffer())
 
 // Image generation
 export default async function Image() {
@@ -43,14 +43,14 @@ export default async function Image() {
             // For convenience, we can re-use the exported opengraph-image
             // size config to also set the ImageResponse's width and height.
             ...size,
-            fonts: [
-                {
-                    name: 'Inter',
-                    data: await interSemiBold,
-                    style: 'normal',
-                    weight: 400,
-                },
-            ],
+            // fonts: [
+            //     {
+            //         name: 'Inter',
+            //         data: await interSemiBold,
+            //         style: 'normal',
+            //         weight: 400,
+            //     },
+            // ],
         }
     )
 }
